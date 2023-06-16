78b145fe64d9fc10f92abb50f7714e44496fda18
umairacode <124578994+umairacode@users.noreply.github.com>
2023-06-16 13:09:31 +0800

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,34 @@
+//clickable page
+function HomePage()
+	{window.location = "home.html"}
+
+function MalaysiaPage()
+	{window.location = "malaysia.html"}
+
+function KLPage()
+	{window.location = "kuala-lumpur.html"}
+
+function AboutPage()
+	{window.location = "about.html"}
+
+function ArticlesPage()
+	{window.location = "articles.html"}
+
+function AKL1()
+	{window.location = "article1.html"}
+
+function AKL2()
+	{window.location = "article2.html"}
+
+function AKL3()
+	{window.location = "article3.html"}
+
+function AKL4()
+	{window.location = "article4.html"}
+	
+function AKL5()
+	{window.location = "article5.html"}
+
+function AKL6()
+	{window.location = "article6.html"}
+
